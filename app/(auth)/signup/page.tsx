aa9c2437f32b0c590f4462571bd532493b9a6ba7
Muhammad Rameez Abid <mrameezabid644@gmail.com>
2024-03-09 19:26:39 +0500

    added inputcard

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -3,11 +3,45 @@ import { Icon } from "common/media/icon";
 import { Button } from "common/widgets/basic/button";
 import { Input } from "common/widgets/basic/input";
 import { Text } from "common/widgets/basic/text";
+import { Flex } from "common/widgets/advance/flex";
 
 export default function SignUpPage() {
   return (
-    <div>
-      <div>Task-2</div>
-    </div>
+    <Flex variant="columnCenterCenter">
+      <Flex
+        variant="columnCenterCenter"
+        className=" px-10 py-10 shadow-2xl w-96 bottom-4 rounded-xl"
+      >
+        <Flex variant="rowStartCenter" className="gap-6">
+          <Button
+            variant="standard"
+            text="Sign In"
+            size="sm"
+            className="text-purple-blue  w-40"
+          />
+          <Button
+            variant="solid-dark-blue"
+            text="Sign Up"
+            size="sm"
+            className="text-white w-40"
+          />
+        </Flex>
+        <Input variant="solid-light-grey" label="First Name" className="w-80" />
+        <Input variant="solid-light-grey" label="Last Name" className="w-80" />
+        <Input variant="solid-light-grey" label="Email" className="w-80" />
+        <Input variant="solid-light-grey" label="Password" className="w-80" />
+        <Input
+          variant="solid-light-grey"
+          label=" Confirm Password"
+          className="w-80"
+        />{" "}
+        <Button
+          variant="solid-dark-blue"
+          text="Sign Up"
+          size="sm"
+          className="text-white w-[100%] mt-16"
+        />
+      </Flex>
+    </Flex>
   );
 }
