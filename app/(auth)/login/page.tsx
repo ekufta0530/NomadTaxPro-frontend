04437d3960e6f46c17c03d055609d5c5ae743f7b
romi <abdulrehman012859@gmail.com>
2024-03-10 11:59:33 +0500

    added login page

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,9 +1,76 @@
 import React from "react";
+import { Icon } from "common/media/icon";
+import { Button } from "common/widgets/basic/button";
+import { Input } from "common/widgets/basic/input";
+import { Text } from "common/widgets/basic/text";
+import { Flex } from "common/widgets/advance/flex";
 
-export default function LoginPage() {
+export default function SignUpPage() {
   return (
-    <div>
-      <div>Login In</div>
-    </div>
+    <Flex variant="columnCenterCenter">
+      <Flex
+        variant="columnCenterCenter"
+        className="p-5 sm:p-10 shadow-2xl w-[calc(100%-1rem)] mt-10  mx-2 sm:w-[30rem]  rounded-xl"
+      >
+        <Flex variant="rowStartCenter" className="gap-10">
+          <Button
+            variant="solid-dark-blue"
+            text="Sign Up"
+            size="md"
+            className="text-white w-32"
+          />
+          <Button
+            variant="standard"
+            text="Sign In"
+            size="sm"
+            className="text-purple-blue"
+          />
+        </Flex>
+        <Text
+          as="h1"
+          className="text-center"
+          text="Create your account!"
+          color="dark-blue"
+          size="2xl"
+          weight="semiBold"
+        />
+        <Input
+          variant="solid-light-grey"
+          label="First Name"
+          className="w-full sm:w-96"
+          containerClass="w-full sm:w-96"
+        />
+        <Input
+          variant="solid-light-grey"
+          label="Last Name"
+          className="w-full sm:w-96"
+          containerClass="w-full sm:w-96"
+        />
+        <Input
+          variant="solid-light-grey"
+          label="Email"
+          className="w-full sm:w-96"
+          containerClass="w-full sm:w-96"
+        />
+        <Input
+          variant="solid-light-grey"
+          label="Password"
+          className="w-full sm:w-96"
+          containerClass="w-full sm:w-96"
+        />
+        <Input
+          variant="solid-light-grey"
+          label=" Confirm Password"
+          className="w-full sm:w-96"
+          containerClass="w-full sm:w-96"
+        />
+        <Button
+          variant="solid-dark-blue"
+          text="Sign Up"
+          size="md"
+          className="text-white w-full sm:w-96 mt-2"
+        />
+      </Flex>
+    </Flex>
   );
 }
