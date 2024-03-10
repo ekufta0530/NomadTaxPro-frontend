ce09468c732747e879ac71afef9983c13691d5ec
romi <abdulrehman012859@gmail.com>
2024-03-10 16:04:46 +0500

    added task-3

---
+++ app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "common/widgets/basic/button";
+import { Input } from "common/widgets/basic/input";
+import { Text } from "common/widgets/basic/text";
+import { Flex } from "common/widgets/advance/flex";
+import Image from "next/image";
+
+export default function Page() {
+  return (
+    <div>
+      <div>Build navbar here</div>
+    </div>
+  );
+}
---
+++ data/static/imgData.ts
@@ -0,0 +1,5 @@
+import logo from "assets/logos/logo.svg";
+
+export const imgs: Record<string, any> = {
+  logo,
+};
