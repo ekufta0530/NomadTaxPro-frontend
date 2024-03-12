3390eb0c2ae02b6b116455146c9535113017429e
Muhammad Rameez Abid <mrameezabid644@gmail.com>
2024-03-12 23:46:51 +0500

    added image and cards

--- data/static/imgData.ts
+++ data/static/imgData.ts
@@ -5,7 +5,7 @@ import algeria from "public/assets/imgs/Algeria.svg";
 import andora from "public/assets/imgs/Andora.svg";
 import angola from "public/assets/imgs/Angola.svg";
 import header from "public/assets/imgs/header.png";
-
+import greece1 from "public/assets/imgs/greece-1.svg";
 export const imgs: Record<string, any> = {
   logo,
   profilePicture,
@@ -14,4 +14,5 @@ export const imgs: Record<string, any> = {
   andora,
   angola,
   header,
+  greece1,
 };
--- sections/dashboard/countryDetail/index.tsx
+++ sections/dashboard/countryDetail/index.tsx
@@ -13,9 +13,210 @@ import { Group } from "common/widgets/advance/group";
 import Image from "next/image";
 
 export function CountryDetail() {
+  const { greece1 } = imgs;
   return (
-    <Flex className="mt-10">
-      <div>Country Detail will go here</div>
+    <Flex variant="rowStartCenter" className="mt-10">
+      <Image src={greece1} alt="greece1" className="w-full h-full" />
+      <div className="px-5 py-5">
+        <Flex
+          variant="columnCenterCenter"
+          className="w-[23rem] h-[23.188rem] rounded-[1.25rem] shadow-grey gap-2 px-10"
+        >
+          <Text
+            as="h1"
+            text="Standard Rates"
+            size="xl"
+            className="bold text-center"
+            color="nile-blue"
+          />
+          <Text
+            as="p"
+            text="These tax rates are indicative and subject to change. Consulting a tax professional is crucial for accurate and personalized advice."
+            size="sm"
+            className="regular text-center px-4 mt-0"
+            color="cold-purple"
+          />
+          <Flex variant="rowBetweenCenter" className="w-full mt-10">
+            <Text
+              as="p"
+              text="Up to €10,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="9%"
+              size="sm"
+              className="bold "
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€10,001 - €20,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="22%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€20,001 - €30,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="28%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€30,001 - €40,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="36%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="Over €40,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="44%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+        </Flex>
+        <Flex
+          variant="columnCenterCenter"
+          className="w-[23rem] h-[19.625rem] rounded-[1.25rem] shadow-grey gap-2 px-10 "
+        >
+          <Text
+            as="h1"
+            text="With 50% Reduction"
+            size="xl"
+            className="bold text-center"
+            color="nile-blue"
+          />
+          <Text
+            as="h1"
+            text="(Estimated)"
+            size="xl"
+            className="bold text-center"
+            color="nile-blue"
+          />
+          <Flex variant="rowBetweenCenter" className="w-full mt-8">
+            <Text
+              as="p"
+              text="Up to €10,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="4.5%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€10,001 - €20,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="11%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€20,001 - €30,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="14%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="€30,001 - €40,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="18%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+          <Flex variant="rowBetweenCenter" className="w-full">
+            <Text
+              as="p"
+              text="Over €40,000"
+              size="sm"
+              className="bold"
+              color="cold-purple"
+            />
+            <Text
+              as="p"
+              text="22%"
+              size="sm"
+              className="bold"
+              color="nile-blue"
+            />
+          </Flex>
+        </Flex>
+      </div>
     </Flex>
   );
 }
