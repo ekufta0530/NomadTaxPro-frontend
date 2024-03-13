795d05df381fc6cf604b3d6cc0d7890b1becdde9
Muhammad Rameez Abid <mrameezabid644@gmail.com>
2024-03-14 02:08:39 +0500

    added picture

--- data/static/imgData.ts
+++ data/static/imgData.ts
@@ -1,11 +1,44 @@
 import logo from "public/assets/logos/logo.svg";
 import profilePicture from "public/assets/imgs/profile-pic.jpeg";
-import albania from "public/assets/imgs/Albania.svg";
-import algeria from "public/assets/imgs/Algeria.svg";
-import andora from "public/assets/imgs/Andora.svg";
-import angola from "public/assets/imgs/Angola.svg";
+import albania from "public/assets/imgs/albania.svg";
+import algeria from "public/assets/imgs/algeria.svg";
+import andora from "public/assets/imgs/andora.svg";
+import angola from "public/assets/imgs/angola.svg";
 import header from "public/assets/imgs/header.png";
 import greece1 from "public/assets/imgs/greece-1.svg";
+import anguilla1 from "public/assets/imgs/countries/anguilla_1.jpeg";
+import anguilla2 from "public/assets/imgs/countries/anguilla_2.jpeg";
+import antiguaBarbuda1 from "public/assets/imgs/countries/antigua barbuda_1.jpeg";
+import barbados1 from "public/assets/imgs/countries/barbados_1.jpeg";
+import barbados2 from "public/assets/imgs/countries/barbados_2.jpeg";
+import bermuda1 from "public/assets/imgs/countries/bermuda_1.jpeg";
+import caymanIcLands1 from "public/assets/imgs/countries/cayman islands_1.jpeg";
+import caymanIcLands2 from "public/assets/imgs/countries/cayman islands_2.jpeg";
+import costaRica1 from "public/assets/imgs/countries/costa rica_1.jpeg";
+import crotia1 from "public/assets/imgs/countries/croatia_1.jpeg";
+import crotia2 from "public/assets/imgs/countries/croatia_2.jpeg";
+import czeckRepublic1 from "public/assets/imgs/countries/czeck republic_1.jpeg";
+import czeckRepublic2 from "public/assets/imgs/countries/czeck republic_2.jpeg";
+import dubai1 from "public/assets/imgs/countries/dubai_1.jpeg";
+import estonia1 from "public/assets/imgs/countries/estonia_1.jpeg";
+import georgia1 from "public/assets/imgs/countries/georgia_1.jpeg";
+import germany1 from "public/assets/imgs/countries/germany_1.jpeg";
+import germany2 from "public/assets/imgs/countries/germany_2.jpeg";
+import icland1 from "public/assets/imgs/countries/iceland_1.jpeg";
+import icland2 from "public/assets/imgs/countries/iceland_2.jpeg";
+import maruritius1 from "public/assets/imgs/countries/mauritius_1.jpeg";
+import maruritius2 from "public/assets/imgs/countries/mauritius_2.jpeg";
+import mexico1 from "public/assets/imgs/countries/mexico_1.jpeg";
+import mexico2 from "public/assets/imgs/countries/mexico_2.jpeg";
+import norway1 from "public/assets/imgs/countries/norway_1.jpeg";
+import norway2 from "public/assets/imgs/countries/norway_2.jpeg";
+import portugal1 from "public/assets/imgs/countries/portugal_1.jpeg";
+import portugal2 from "public/assets/imgs/countries/portugal_2.jpeg";
+import spain1 from "public/assets/imgs/countries/spain_1.jpeg";
+import thailand1 from "public/assets/imgs/countries/thailand_1.jpeg";
+import thailand2 from "public/assets/imgs/countries/thailand_2.jpeg";
+import uruguay1 from "public/assets/imgs/countries/uruguay_1.jpeg";
+
 export const imgs: Record<string, any> = {
   logo,
   profilePicture,
@@ -15,4 +48,36 @@ export const imgs: Record<string, any> = {
   angola,
   header,
   greece1,
+  anguilla1,
+  anguilla2,
+  antiguaBarbuda1,
+  barbados1,
+  barbados2,
+  bermuda1,
+  caymanIcLands1,
+  caymanIcLands2,
+  costaRica1,
+  crotia1,
+  crotia2,
+  czeckRepublic1,
+  czeckRepublic2,
+  dubai1,
+  estonia1,
+  georgia1,
+  germany1,
+  germany2,
+  icland1,
+  icland2,
+  maruritius1,
+  maruritius2,
+  mexico1,
+  mexico2,
+  norway1,
+  norway2,
+  portugal1,
+  portugal2,
+  spain1,
+  thailand1,
+  thailand2,
+  uruguay1,
 };
--- sections/dashboard/countryDetail/index.tsx
+++ sections/dashboard/countryDetail/index.tsx
@@ -15,208 +15,273 @@ import Image from "next/image";
 export function CountryDetail() {
   const { greece1 } = imgs;
   return (
-    <Flex variant="rowStartCenter" className="mt-10">
-      <Image src={greece1} alt="greece1" className="w-full h-full" />
-      <div className="px-5 py-5">
-        <Flex
-          variant="columnCenterCenter"
-          className="w-[23rem] h-[23.188rem] rounded-[1.25rem] shadow-grey gap-2 px-10"
-        >
-          <Text
-            as="h1"
-            text="Standard Rates"
-            size="xl"
-            className="bold text-center"
-            color="nile-blue"
-          />
-          <Text
-            as="p"
-            text="These tax rates are indicative and subject to change. Consulting a tax professional is crucial for accurate and personalized advice."
-            size="sm"
-            className="regular text-center px-4 mt-0"
-            color="cold-purple"
-          />
-          <Flex variant="rowBetweenCenter" className="w-full mt-10">
+    <div>
+      <Flex variant="rowStartCenter" className="mt-10">
+        <Image src={greece1} alt="greece1" className="w-full h-full" />
+        <div className="px-5 py-5">
+          <Flex
+            variant="columnCenterCenter"
+            className="w-[23rem] h-[23.188rem] rounded-[1.25rem] shadow-grey gap-2 px-10"
+          >
             <Text
-              as="p"
-              text="Up to €10,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="9%"
-              size="sm"
-              className="bold "
+              as="h1"
+              text="Standard Rates"
+              size="xl"
+              className="bold text-center"
               color="nile-blue"
             />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
             <Text
               as="p"
-              text="€10,001 - €20,000"
+              text="These tax rates are indicative and subject to change. Consulting a tax professional is crucial for accurate and personalized advice."
               size="sm"
-              className="bold"
+              className="regular text-center px-4 mt-0"
               color="cold-purple"
             />
+            <Flex variant="rowBetweenCenter" className="w-full mt-10">
+              <Text
+                as="p"
+                text="Up to €10,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="9%"
+                size="sm"
+                className="bold "
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€10,001 - €20,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="22%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€20,001 - €30,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="28%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€30,001 - €40,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="36%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="Over €40,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="44%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+          </Flex>
+          <Flex
+            variant="columnCenterCenter"
+            className="w-[23rem] h-[19.625rem] rounded-[1.25rem] shadow-grey gap-2 px-10 "
+          >
             <Text
-              as="p"
-              text="22%"
-              size="sm"
-              className="bold"
+              as="h1"
+              text="With 50% Reduction"
+              size="xl"
+              className="bold text-center"
               color="nile-blue"
             />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
             <Text
-              as="p"
-              text="€20,001 - €30,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="28%"
-              size="sm"
-              className="bold"
+              as="h1"
+              text="(Estimated)"
+              size="xl"
+              className="bold text-center"
               color="nile-blue"
             />
+            <Flex variant="rowBetweenCenter" className="w-full mt-8">
+              <Text
+                as="p"
+                text="Up to €10,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="4.5%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€10,001 - €20,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="11%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€20,001 - €30,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="14%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="€30,001 - €40,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="18%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
+            <Flex variant="rowBetweenCenter" className="w-full">
+              <Text
+                as="p"
+                text="Over €40,000"
+                size="sm"
+                className="bold"
+                color="cold-purple"
+              />
+              <Text
+                as="p"
+                text="22%"
+                size="sm"
+                className="bold"
+                color="nile-blue"
+              />
+            </Flex>
           </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="€30,001 - €40,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="36%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="Over €40,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="44%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-        </Flex>
-        <Flex
-          variant="columnCenterCenter"
-          className="w-[23rem] h-[19.625rem] rounded-[1.25rem] shadow-grey gap-2 px-10 "
-        >
-          <Text
-            as="h1"
-            text="With 50% Reduction"
-            size="xl"
-            className="bold text-center"
-            color="nile-blue"
-          />
-          <Text
-            as="h1"
-            text="(Estimated)"
-            size="xl"
-            className="bold text-center"
-            color="nile-blue"
-          />
-          <Flex variant="rowBetweenCenter" className="w-full mt-8">
-            <Text
-              as="p"
-              text="Up to €10,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="4.5%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="€10,001 - €20,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="11%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="€20,001 - €30,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="14%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="€30,001 - €40,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="18%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-          <Flex variant="rowBetweenCenter" className="w-full">
-            <Text
-              as="p"
-              text="Over €40,000"
-              size="sm"
-              className="bold"
-              color="cold-purple"
-            />
-            <Text
-              as="p"
-              text="22%"
-              size="sm"
-              className="bold"
-              color="nile-blue"
-            />
-          </Flex>
-        </Flex>
-      </div>
-    </Flex>
+        </div>
+      </Flex>
+      <Text
+        size="xl"
+        as="h1"
+        weight="bold"
+        color="nile-blue"
+        className="mt-10 leading-tight"
+      >
+        Greece: A Premier Destination for Digital Nomads Remember: Consult a Tax
+        <br /> Professional for Personalized Advice!
+      </Text>
+      <Text
+        as="h3"
+        text="Embark on Your Greek Odyssey: The Ideal Digital Nomad Destination"
+        size="lg"
+        weight="bold"
+        color="dark-grey"
+        className="mt-10 leading-tight"
+      />
+      <Text
+        as="p"
+        text="Discover Greece, not just as a country but as an idyllic lifestyle choice for digital nomads. Renowned as a perfect anchor country, Greece marries financial incentives with a lifestyle that's both enriching and relaxing, making it a top choice for remote workers from across the globe."
+        size="sm"
+        weight="regular"
+        color="dark-grey"
+        className="mt-10 leading-tight"
+      />
+      <Text
+        as="p"
+        text="Financial Benefits Tailored for Digital Nomads: Greece rolls out the red carpet for digital nomads with its distinct financial advantages:"
+        size="sm"
+        weight="regular"
+        color="dark-grey"
+        className="mt-10 leading-tight"
+      />
+      <Text
+        as="h1"
+        text="Financial Benefits"
+        size="xl"
+        weight="bold"
+        color="nile-blue"
+        className="mt-10 leading-tight"
+      />
+      <Flex
+        variant="columnCenterCenter"
+        className="w-[23.188rem] h-[17.375rem] shadow-grey rounded-[1.25rem]"
+      >
+        <Icon icon="Sun" size="sm" />
+        <Text
+          as="h1"
+          text="Sun-Kissed Climate"
+          size="lg"
+          weight="bold"
+          color="nile-blue"
+        />
+        <Text
+          as="p"
+          text="Greece's Mediterranean climate promises sunny days, ideal for both productivity and relaxation."
+          size="xs"
+          weight="medium"
+          color="cold-purple"
+          className="px-12 text-center"
+        />
+      </Flex>
+    </div>
   );
 }
