5fb85cd72554ccf595f8ccb053114f5cc281ea3e
romi <abdulrehman012859@gmail.com>
2024-03-14 20:40:11 +0500

    added Task-7

---
+++ app/(dashboard)/favorites/page.tsx
@@ -0,0 +1,10 @@
+import { FavoriteCountryList } from "components/lists/favoriteCountryList";
+import React from "react";
+
+export default function Page() {
+  return (
+    <div>
+      <FavoriteCountryList />
+    </div>
+  );
+}
