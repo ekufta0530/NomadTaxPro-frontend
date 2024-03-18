bb4e1acba03c4ba26d83476f57bec38d4e59645a
Muhammad Rameez Abid <mrameezabid644@gmail.com>
2024-03-19 00:10:18 +0500

    added card

--- app/(dashboard)/task-10/page.tsx
+++ app/(dashboard)/task-10/page.tsx
@@ -14,8 +14,76 @@ import Image from "next/image";
 
 export default function page() {
   return (
-    <div>
-      <div>task-10</div>
+    <div className="w-[21.875rem] shadow-grey rounded-[1.875rem]">
+      <Flex variant="rowBetweenCenter" className="px-4 py-4 ">
+        <Text
+          as="h1"
+          size="lg"
+          text="Schedule"
+          color="nile-blue"
+          weight="bold"
+        />
+        <Flex variant="rowStartCenter" className="gap-2">
+          <Text
+            as="p"
+            size="sm"
+            text="View all "
+            color="purple-blue"
+            weight="medium"
+          />
+          <Icon icon="KeyboardRightBlue" size="xs" />
+        </Flex>
+      </Flex>
+      <Flex variant="columnCenterCenter" className="gap-2">
+        <Text
+          as="h1"
+          size="lg"
+          text="Foreign Days"
+          color="nile-blue"
+          weight="bold"
+        />
+        <Text
+          as="p"
+          size="sm"
+          text="Discover your profit, and learn more about your business "
+          color="cold-purple"
+          weight="medium"
+          className="w-[15rem] text-center mt-0"
+        />
+      </Flex>
+      <Flex variant="rowStartCenter" className=" mt-10 px-5 py-5   ">
+        <Flex variant="columnStartStart" className="gap-1">
+          <Text
+            as="p"
+            size="sm"
+            text="Est. Total"
+            color="cold-purple"
+            weight="medium"
+          />
+          <Text as="h1" size="lg" text="1540" color="nile-blue" weight="bold" />
+        </Flex>
+        <Flex
+          variant="columnStartStart"
+          className="gap-1 border-l border-light-grey ml-7"
+        >
+          <Text
+            as="p"
+            size="sm"
+            text="Est. Tax"
+            color="cold-purple"
+            weight="medium"
+            className="ml-5"
+          />
+          <Text
+            as="h1"
+            size="lg"
+            text="$3.984"
+            color="nile-blue"
+            weight="bold"
+            className="ml-5"
+          />
+        </Flex>
+      </Flex>
     </div>
   );
 }
