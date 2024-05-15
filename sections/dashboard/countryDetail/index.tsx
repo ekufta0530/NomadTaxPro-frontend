3b7f978a52920408b3a4294215da750a2cbe0a2d
romi <abdulrehman012859@gmail.com>
2024-05-15 10:25:59 +0500

    remvoed btn group

--- sections/dashboard/countryDetail/index.tsx
+++ sections/dashboard/countryDetail/index.tsx
@@ -1,15 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import React from "react";
 import { Flex } from "common/widgets/advance/flex";
-import { Icon } from "common/media/icon";
-import { Select } from "common/widgets/basic/select";
-import { Input } from "common/widgets/basic/input";
 import { Text } from "common/widgets/basic/text";
-import { Button } from "common/widgets/basic/button";
-import { Img } from "common/media/img";
-import { imgs } from "data/static/imgData";
-import { Map } from "components/map";
-import { Group } from "common/widgets/advance/group";
 import Image from "next/image";
 import { Benefits } from "./benefits";
 
